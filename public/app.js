9a93ff48c9aef854d32ad9ad14f3a0c2eafe235d
jayxu66 <sgliv8@gmail.com>
2015-07-16 23:32:05 -0700

    Set up basic server and angular for json web token login

--- public/app.js
+++ public/app.js
@@ -4,15 +4,27 @@
 
 	//app.constant('API_URL', 'http://localhost:3000');
 
-	app.controller('MainCtrl', function MainCtrl(RandomUserFactory){
+	app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory){
 		'use strict';
 		var vm = this;
 		vm.getRandomUser = getRandomUser;
+		vm.login = login;
 
 		function getRandomUser() {
 			RandomUserFactory.getUser().then(function success(response){
 				vm.randomUser = response.data;
 			});
+		}
+
+		function login(username, password){
+			UserFactory.login(username, password).then(function success(response){
+				vm.user = response.data.user;
+				alert(response.data.token);
+			}, handleError);
+		}
+
+		function handleError(response){
+			alert('Error:' + response.data);
 		}
 	});
 
@@ -26,5 +38,20 @@
 			return $http.get('/random-user');
 		}
 	});
+
+	app.factory('UserFactory', function($http){
+
+		'use strict'
+		return {
+			login: login
+		}
+
+		function login(username, password){
+			return $http.post('/login', {
+				username: username,
+				password: password
+			});
+		}
+	})
 
 })();
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
-var path = require('path');
 
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var jwt = require('jsonwebtoken');
+
+var jwtSercet = 'fdsfafdhoijiafd/3uk';
+
+var user = {
+	username: 'jayemily',
+	password: 'p'
+};
 
 var app = express();
 
 //app.use(cors());
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -16,6 +28,33 @@ app.get('/random-user', function(req, res){
 	res.json(user);
 });
 
+app.post('/login', authenticate, function(req, res){
+	var token = jwt.sign({
+		username: user.username
+	}, jwtSercet);
+	res.send({
+		token: token,
+		user: user
+	});
+});
+
+
 app.listen(3000, function(){
 	console.log("App listening on localhost:3000");
 });
+
+// UTIL FUNCTIONS
+
+function authenticate(req, res, next){
+	var body = req.body;
+
+	if(!body.username || !body.password){
+		res.status(400).end('Must provide username or password');
+	}
+
+	if( body.username !== user.username || body.password !== user.password){
+		res.status(401).end('Username or password incorrect');
+	}
+
+	next();
+}
